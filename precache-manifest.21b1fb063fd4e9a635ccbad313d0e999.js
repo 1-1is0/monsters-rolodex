f4282c2149713a8332f1c278d39d5f225d2bbf60
amirftma <amirftma@gmail.com>
2019-08-20 11:09:07 +0430

    Updates

---
+++ precache-manifest.21b1fb063fd4e9a635ccbad313d0e999.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85e29e73d968f4be2f369e16727eced0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "49fe92536343c1ad4981",
+    "url": "/monsters-rolodex/static/css/main.be509aa1.chunk.css"
+  },
+  {
+    "revision": "622b344709d4c344c48c",
+    "url": "/monsters-rolodex/static/js/2.37ea2c95.chunk.js"
+  },
+  {
+    "revision": "49fe92536343c1ad4981",
+    "url": "/monsters-rolodex/static/js/main.273ff641.chunk.js"
+  },
+  {
+    "revision": "9ea9c29140c1f80c8044",
+    "url": "/monsters-rolodex/static/js/runtime~main.3810dc4c.js"
+  }
+]);
