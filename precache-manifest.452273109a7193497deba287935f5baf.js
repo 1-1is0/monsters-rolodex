1df7b878ad64369536220fa8d577670f41358767
amirftma <amirftma@gmail.com>
2019-08-20 10:44:00 +0430

    Updates

---
+++ precache-manifest.452273109a7193497deba287935f5baf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb328070e6cf029c401ec4a9ca0ec3da",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "d06a27127abb8d737825",
+    "url": "/monster-rolodex/static/css/main.6c1a1d06.chunk.css"
+  },
+  {
+    "revision": "622b344709d4c344c48c",
+    "url": "/monster-rolodex/static/js/2.37ea2c95.chunk.js"
+  },
+  {
+    "revision": "d06a27127abb8d737825",
+    "url": "/monster-rolodex/static/js/main.a4d0cb50.chunk.js"
+  },
+  {
+    "revision": "384d151a98451564c735",
+    "url": "/monster-rolodex/static/js/runtime~main.50883f11.js"
+  }
+]);
